3567eefc0c9c1aaa87424260c924974359e94018
Matthew Schuelke <matt@themadstatter.com>
2020-10-30 15:41:35 -0500

    db insert fns

--- R/consult_database.R
+++ R/consult_database.R
@@ -1,14 +1,21 @@
-#' Copy user information from ldap to clipboard
-#' 
-#' @description Look up a user in ldap by email and copy data to clipboard to
-#' paste into the consult database.
-#' 
+################################################################################
+# USER TABLE FUNCTIONS
+################################################################################
+
+#' Populate a tibble with new user information from LDAP
+#'
 #' @param email mail of the user
-#' 
-#' @export
-db_clip_user <- function(email) {
+#' @param user_id primary key of user
+#' @param icts integer indicating icts membership status
+#' @param app_role role of the user
+#'
+#' @return a tibble with populated user information
+db_new_user <- function(email, user_id, icts, app_role = c("user", "admin")) {
+  app_role <- match.arg(app_role)
+  
   q <- washu::wu_ldap_query("mail", email)
-  v <- c("cn",
+  v <- c("user_id",
+         "cn",
          "sn",
          "title",
          "physicalDeliveryOfficeName",
@@ -23,14 +30,123 @@ db_clip_user <- function(email) {
          "userPrincipalName",
          "mail",
          "eduPersonNickname",
-         "eduPersonPrimaryAffiliation")
+         "eduPersonPrimaryAffiliation",
+         "app_role",
+         "icts")
   for(m in setdiff(v, names(q))) {
     q[m] <- NA
   }
   q <- q[v]
-  q$app_role <- "user"
-  clipr::write_clip(q)
+  q$user_id <- user_id
+  q$app_role <- app_role
+  q$icts <- icts
+  q
 }
+
+#' Construct SQL insert syntax for a new user
+#'
+#' @inheritParams db_new_user
+#'
+#' @return SQL syntax to insert a new user
+db_sql_insert_user_single <- function(email, user_id, icts, app_role = "user") {
+  q <- db_new_user(email, user_id, icts, app_role)
+  sprintf("INSERT INTO wustl_consults.user VALUES ('%s');", paste(q, collapse = "', '"))
+}
+
+#' Construct SQL insert syntax for new users
+#'
+#' @inheritParams db_new_user
+#'
+#' @return SQL syntax to insert new users
+db_sql_insert_user <- function(email, user_id, icts, app_role) {
+  d <- dplyr::tibble(email, user_id, icts, app_role)
+  purrr::pmap_chr(d, db_sql_insert_user_single)
+}
+
+#' Copy SQL insert syntax for new users to the clipboard
+#'
+#' @inheritParams db_new_user
+#'
+#' @export
+#' 
+#' @examples 
+#' \dontrun{
+#' db_sql_insert_user_clip(email = c("poo@wustl.edu", "piglet@wustl.edu"),
+#'                         user_id = 1:2,
+#'                         icts = c(1, 0),
+#'                         app_role = c("admin", "user"))
+#' }
+db_sql_insert_user_clip <- function(email, user_id, icts, app_role) {
+  sql <- db_sql_insert_user(email, user_id, icts, app_role)
+  clipr::write_clip(sql)
+}
+
+#' Copy data in SQL insert syntax for new users to the clipboard
+#' 
+#' @description Look up a user in ldap by email and copy data to clipboard to
+#' paste into the consult database.
+#' 
+#' @inheritParams db_new_user
+#' 
+#' @export
+db_clip_user <- function(email) {
+  .Deprecated("db_sql_insert_user_clip")
+  u <- db_new_user(email, 1, 1)
+  clipr::write_clip(u)
+}
+
+
+
+################################################################################
+# USER_CONSULT TABLE FUNCTIONS
+################################################################################
+
+#' Construct SQL insert syntax for a new user-consult pairing
+#'
+#' @param user_consult_id primary key for table
+#' @param consult_id consult id including version number
+#' @param user_id foreign key for user from user table
+#' @param role role of the user on the consult
+#'
+#' @return SQL syntax to insert a new user-consult pairing
+db_sql_insert_user_consult_single <- function(user_consult_id, consult_id, user_id, role = c("principal", "statistician", "student", "resident", "coinvestigator")) {
+  role <- match.arg(role)
+  d <- c(user_consult_id, consult_id, user_id, role)
+  sprintf("INSERT INTO wustl_consults.user_consult VALUES ('%s');", paste(d, collapse = "', '"))
+}
+
+#' Construct SQL insert syntax for new user-consult pairings
+#'
+#' @inheritParams db_sql_insert_user_consult_single
+#'
+#' @return SQL syntax to insert new users
+db_sql_insert_user_consult <- function(user_consult_id, consult_id, user_id, role) {
+  purrr::pmap_chr(dplyr::tibble(user_consult_id, consult_id, user_id, role), db_sql_insert_user_consult_single)
+}
+
+#' Copy SQL insert syntax for new user-consult pairings to the clipboard
+#'
+#' @inheritParams db_sql_insert_user_consult_single
+#'
+#' @export
+#' 
+#' @examples 
+#' \dontrun{
+#' db_sql_insert_user_consult_clip(user_consult_id = 57:58, 
+#'                                 consult_id = "2020-11-01-1-v1", 
+#'                                 user_id = c(1, 14), 
+#'                                 role = c("statistician", "principal"))
+#' }
+db_sql_insert_user_consult_clip <- function(user_consult_id, consult_id, user_id, role) {
+  sql <- db_sql_insert_user_consult(user_consult_id, consult_id, user_id, role)
+  clipr::write_clip(sql)
+}
+
+
+
+################################################################################
+# SHINY APP
+################################################################################
 
 #' Run shiny app to search consult database
 #'
