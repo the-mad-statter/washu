aedb8c3cc47a70b76cfa209eda74e04c1629ff8a
Matthew Schuelke <matt@themadstatter.com>
2021-01-19 10:20:42 -0600

    select_via_cor_sig()

--- R/broom_tidy.R
+++ R/broom_tidy.R
@@ -6,7 +6,7 @@
 #' @return a data frame with an additional character column named p_flag
 #' @export
 add_significance_flag <- function(data, p_val_col) {
-  data %>% 
+  data %>%
     dplyr::mutate(p_flag = dplyr::case_when({{ p_val_col }} < .001 ~ "***",
                                             {{ p_val_col }} < .010 ~ "**",
                                             {{ p_val_col }} < .050 ~ "*",
@@ -15,7 +15,7 @@ add_significance_flag <- function(data, p_val_col) {
 }
 
 #' Pairwise t tests
-#' 
+#'
 #' @description Performs independent sample t-tests for each pair of groups from tidy data.
 #'
 #' @param data tidy data containing the response and group variables
@@ -28,38 +28,38 @@ add_significance_flag <- function(data, p_val_col) {
 broom_tidy_pairwise_t_test_two_sample <- function(data, response, group, ...) {
   response <- rlang::ensym(response)
   group <- rlang::ensym(group)
-  
-  data %>% 
-    dplyr::pull(!!group) %>% 
-    factor() %>% 
+
+  data %>%
+    dplyr::pull(!!group) %>%
+    factor() %>%
     levels() -> conditions
-  
-  combn(1:length(conditions), 2) %>% 
-    split(rep(1:ncol(.), each = nrow(.))) %>% 
+
+  combn(1:length(conditions), 2) %>%
+    split(rep(1:ncol(.), each = nrow(.))) %>%
     lapply(function(p) {
       # make subset
-      data %>% 
+      data %>%
         dplyr::filter(!!group %in% conditions[p]) -> ss
-      
+
       # model
       tt <- rlang::eval_tidy(rlang::quo(t.test(!!response ~ !!group, data = ss, ...)))
-      
+
       # tidy
-      tt %>% 
-        broom::tidy() %>% 
+      tt %>%
+        broom::tidy() %>%
         dplyr::mutate(
           "response" = rlang::as_string(response),
           "group1" = !!paste0(group, "==", conditions[p][1]),
           "group2" = !!paste0(group, "==", conditions[p][2])
-        ) %>% 
+        ) %>%
         dplyr::select(response, group1, group2, dplyr::everything())
-    }) %>% 
-    dplyr::bind_rows() %>% 
+    }) %>%
+    dplyr::bind_rows() %>%
     add_significance_flag(p.value)
 }
 
 #' Pairwise t tests
-#' 
+#'
 #' @description Performs paired t-tests for each pair of groups where observations are paired by unit from tidy data.
 #'
 #' @param data tidy data containing the response, group, and unit variables
@@ -67,7 +67,7 @@ broom_tidy_pairwise_t_test_two_sample <- function(data, response, group, ...) {
 #' @param group grouping variable
 #' @param unit sampling unit variable (e.g., persons)
 #' @param ... additional parameters passed onto \code{\link[stats:t.test]{t.test()}}
-#' 
+#'
 #' @note Tests are performed on pairwise complete data.
 #'
 #' @return a tibble with information about the model components; one model per row
@@ -76,50 +76,50 @@ broom_tidy_pairwise_t_test_dep_sample <- function(data, response, group, unit, .
   response <- rlang::ensym(response)
   group <- rlang::ensym(group)
   unit <- rlang::ensym(unit)
-  
-  data %>% 
-    dplyr::pull(!!group) %>% 
-    factor() %>% 
+
+  data %>%
+    dplyr::pull(!!group) %>%
+    factor() %>%
     levels() -> conditions
-  
-  combn(1:length(conditions), 2) %>% 
-    split(rep(1:ncol(.), each = nrow(.))) %>% 
+
+  combn(1:length(conditions), 2) %>%
+    split(rep(1:ncol(.), each = nrow(.))) %>%
     lapply(function(p) {
       # make pairwise complete subset
-      data %>% 
-        dplyr::filter(!is.na(!!response), !!group %in% conditions[p]) %>% 
-        dplyr::group_by(!!unit) %>% 
-        dplyr::filter(n() == 2) %>% 
+      data %>%
+        dplyr::filter(!is.na(!!response), !!group %in% conditions[p]) %>%
+        dplyr::group_by(!!unit) %>%
+        dplyr::filter(n() == 2) %>%
         dplyr::ungroup() -> data_pairwise_complete
-      
+
       # split into (x,y)
-      data_pairwise_complete %>% 
-        dplyr::filter(!!group == conditions[p][1]) %>% 
+      data_pairwise_complete %>%
+        dplyr::filter(!!group == conditions[p][1]) %>%
         dplyr::pull(!!response) -> x
-      
-      data_pairwise_complete %>% 
-        dplyr::filter(!!group == conditions[p][2]) %>% 
+
+      data_pairwise_complete %>%
+        dplyr::filter(!!group == conditions[p][2]) %>%
         dplyr::pull(!!response) -> y
-      
+
       # model
       tt <- t.test(x, y, paired = TRUE, ...)
-      
+
       # tidy
-      tt %>% 
-        broom::tidy() %>% 
+      tt %>%
+        broom::tidy() %>%
         dplyr::mutate(
           "response" = rlang::as_string(response),
           "group1" = !!paste0(group, "==", conditions[p][1]),
           "group2" = !!paste0(group, "==", conditions[p][2])
-        ) %>% 
+        ) %>%
         dplyr::select(response, group1, group2, dplyr::everything())
-    }) %>% 
-    dplyr::bind_rows() %>% 
+    }) %>%
+    dplyr::bind_rows() %>%
     add_significance_flag(p.value)
 }
 
 #' Pairwise t tests
-#' 
+#'
 #' @description Performs one-sample t-tests for each group from tidy data.
 #'
 #' @param data tidy data containing the response and group variables
@@ -133,37 +133,37 @@ broom_tidy_pairwise_t_test_dep_sample <- function(data, response, group, unit, .
 broom_tidy_pairwise_t_test_one_sample <- function(data, response, group, mu = 0, ...) {
   response <- rlang::ensym(response)
   group <- rlang::ensym(group)
-  
-  data %>% 
-    dplyr::pull(!!group) %>% 
-    factor() %>% 
+
+  data %>%
+    dplyr::pull(!!group) %>%
+    factor() %>%
     levels() -> conditions
-  
-  conditions %>% 
+
+  conditions %>%
     lapply(function(p) {
       # make subset
-      data %>% 
-        dplyr::filter(!!group == p) %>% 
+      data %>%
+        dplyr::filter(!!group == p) %>%
         dplyr::pull(!!response) -> x
-      
+
       # model
       tt <- t.test(x, mu = mu, ...)
-      
+
       # tidy
-      tt %>% 
-        broom::tidy() %>% 
+      tt %>%
+        broom::tidy() %>%
         dplyr::mutate(
           "response" = rlang::as_string(response),
           "group" = !!paste0(group, "==", p),
-        ) %>% 
+        ) %>%
         dplyr::select(response, group, dplyr::everything())
-    }) %>% 
-    dplyr::bind_rows() %>% 
+    }) %>%
+    dplyr::bind_rows() %>%
     add_significance_flag(p.value)
 }
 
 #' Pairwise cor tests
-#' 
+#'
 #' @description Test for association between paired samples, using one of Pearson's product moment correlation coefficient, Kendall's tau or Spearman's rho for each pair of variables.
 #'
 #' @param data data frame containing the variables
@@ -173,25 +173,63 @@ broom_tidy_pairwise_t_test_one_sample <- function(data, response, group, mu = 0,
 #' @export
 broom_tidy_cor_test <- function(data, ...) {
   vars <- names(data)
-  combn(1:length(vars), 2) %>% 
-    split(rep(1:ncol(.), each = nrow(.))) %>% 
+  combn(1:length(vars), 2) %>%
+    split(rep(1:ncol(.), each = nrow(.))) %>%
     lapply(function(p) {
       # split into (x, y)
       x <- data[[vars[p[1]]]]
       y <- data[[vars[p[2]]]]
-      
+
       # model
       ct <- cor.test(x, y, ...)
-      
+
       # tidy
       ct %>%
-        broom::tidy() %>% 
+        broom::tidy() %>%
         dplyr::mutate(
           v1 = vars[p][1],
           v2 = vars[p][2]
-        ) %>% 
+        ) %>%
         dplyr::select(v1, v2, dplyr::everything())
-    }) %>% 
-    dplyr::bind_rows() %>% 
+    }) %>%
+    dplyr::bind_rows() %>%
     add_significance_flag(p.value)
+}
+
+#' Subset columns using a correlation p-value
+#'
+#' @description Select variables in a data frame based on the p-value of the correlation with one of the variables.
+#'
+#' @param data A data frame, data frame extension (e.g. a tibble), or a lazy data frame (e.g. from dbplyr or dtplyr).
+#' @param x name of the common variable with which to compute correlations
+#' @param p.value the maximum desired p-value
+#' @param ... additional arguments passed to \link[stats]{cor.test}
+#'
+#' @return An object of the same type as .data
+#' @export
+#'
+#' @examples
+#' select_via_cor_sig(mtcars, mpg, 0.001)
+select_via_cor_sig <- function(.data, x, p.value, ...) {
+  x <- rlang::ensym(x)
+
+  .data %>%
+    select(-x) %>%
+    names() %>%
+    lapply(function(candidate) {
+      c(rlang::as_string(x), candidate)
+    }) %>%
+    lapply(function(pair) {
+      x <- .data[[pair[1]]]
+      y <- .data[[pair[2]]]
+
+      cor.test(x, y, ...) %>%
+        broom::tidy() %>%
+        dplyr::mutate(v1 = pair[1], v2 = pair[2]) %>%
+        dplyr::select(v1, v2, dplyr::everything())
+    }) %>%
+    dplyr::bind_rows() %>%
+    dplyr::filter(p.value < {{p.value}}) %>%
+    dplyr::pull(v2) %>%
+    { suppressMessages(dplyr::select(.data, dplyr::all_of(c(rlang::as_string(x), .)))) }
 }
---
+++ R/table1.R
@@ -0,0 +1,59 @@
+#' Generate an HTML table of descriptive statistics
+#'
+#' @param file optional file location to write table to
+#' @param css cascading style sheet to embed
+#' @inheritParams table1::table1
+#'
+#' @return a washu_table1 object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' table1(~ ., mtcars, "Table 1. Descriptives.html")
+#' }
+table1 <- function(
+  x,
+  data,
+  file,
+  css = system.file("table1_defaults_1.0",
+                    "table1_defaults.css",
+                    package = "table1"),
+  ...) {
+  table1::table1(x, data, ...) -> table_1
+
+  html_lines <- c("<html>",
+                  "<style>",
+                  readLines(css),
+                  "</style>",
+                  table_1,
+                  "</html>")
+
+  if(!missing(file))
+    writeLines(html_lines, file)
+
+  structure(html_lines,
+            class = c("washu_table1", class(html_lines)))
+}
+
+#' Print washu_table1 objects
+#'
+#' @param x an object of type washu_table1
+#'
+#' @return the argument x, invisibly
+#' 
+#' @export
+print.washu_table1 <- function(x) {
+  tf <- tempfile(fileext = ".html")
+  cat(x, file = tf)
+  rstudioapi::viewer(tf)
+  invisible(x)
+}
+
+#' Custom S3 Print for washu_table1 objects
+#' @param x A washu_table1 object to print
+#' @param ... additional arguments
+#' @importFrom knitr knit_print
+#' @export
+knit_print.washu_table1 <- function(x, ...) {
+    knitr::asis_output(x)
+}
