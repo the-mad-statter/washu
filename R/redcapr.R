057633c642243bc93b23495ea566d61f74325d58
Matthew Schuelke <matt@themadstatter.com>
2021-05-06 10:04:08 -0500

    retrieve_credential_local()

---
+++ R/redcapr.R
@@ -0,0 +1,10 @@
+#' @inherit REDCapR::retrieve_credential
+retrieve_credential_local <- function(project_id,
+                                      path_credential = "~/.REDCapR",
+                                      ...) {
+  REDCapR::retrieve_credential_local(
+    path_credential = path_credential,
+    project_id = project_id,
+    ...
+  )
+}
